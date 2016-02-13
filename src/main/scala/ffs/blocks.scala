e1c1bc91b6d6a8a39d6132a4d000ab73955ee5a2
Justin Kaeser <github@accounts.justinkaeser.com>
2016-02-13 16:42:50 +0100

    a little better idea how to structure FFS layout. Now inspired by unix file system!

--- src/main/scala/ffs/blocks.scala
+++ src/main/scala/ffs/blocks.scala
@@ -7,7 +7,7 @@ import java.nio.ByteBuffer
 sealed abstract class Block {
 
   /** Allocate a buffer with standard size for the FFS format. */
-  def buffer(): ByteBuffer = ByteBuffer.allocate(512)
+  def buffer(): ByteBuffer = ByteBuffer.allocate(blocks.blockSize)
   def toBytes: Array[Byte]
 }
 
@@ -23,35 +23,44 @@ case class HeaderBlock(rootBlockAddresses: Vector[Int]) extends Block {
 
   override def toBytes = {
     val b = buffer()
-    b.putShort(format) // 2b
+    b.putShort(magic) // 2b
     b.put(version) // 1b
 
-    // assuming maximum size fits into a byte...
+    // the root block gets up to 127 directory blocks
     // TODO handle too many root blocks errors
     b.put(rootBlockAddresses.size.toByte) // 1b
 
-    // leaves us 509 bytes, space for 254 2-byte bock addresses
-
+    // leaves us 508 bytes, space for 127 4-byte block addresses
     rootBlockAddresses.foreach { a =>
-      b.putShort(address(a)) // 2b quasi-unsigned short each
+      b.putInt(a) // 4B
     }
 
     b.array()
   }
 }
 
-case class LayoutBlock(files: Vector[Node]) extends Block {
+/** A file block represents either a regular file with data or a directory. */
+case class FileBlock(dataBlocks: Vector[Block]) extends Block {
   override def toBytes = {
-    // layout block marker
+    // file size in bytes (4B)
+
+    ???
+  }
+}
+
+/** A directory block contains data about files and directories within a directory. */
+case class DirectoryBlock(files: Vector[Entry]) extends Block {
+  override def toBytes = {
+    //
     // sequence of files and dirs
     // dirs:
-      // dir marker (1B)
+      // flags (1B)
       // dir name (8B)
-      // dir block count
-      // dir layout blocks
+      // dir block address (4B)
     // files:
-      // file marker (1B)
+      // flags (1B)
       // file name (8B)
+      // file block address (4B)
       // file data total size in bytes (4B)
       // file data block addresses (2B * fileSize/512)
 
@@ -61,34 +70,49 @@ case class LayoutBlock(files: Vector[Node]) extends Block {
   }
 }
 
+
+
 case class DataBlock(data: Array[Byte]) extends Block {
   override def toBytes = data
 }
 
-sealed abstract class Node {
+sealed abstract class Entry {
   val name: String
 
-  def marker: Byte // wasteful, I know :)
   def toBytes: Array[Byte]
 }
 
-case class DirNode(name: String, blocks: Vector[Int]) extends Node {
-  override def marker = 0.toByte
+case class DirEntry(name: String, deleted: Boolean, blocks: Vector[Int]) extends Entry {
   override def toBytes = ???
 }
 
-case class FileNode(name: String, size: Int, addresses: Vector[Int]) extends Node {
-  override def marker = 1.toByte
+case class FileEntry(name: String, deleted: Boolean, size: Int, addresses: Vector[Int]) extends Entry {
   override def toBytes: Array[Byte] = ???
 }
 
 object blocks {
 
+  object flags {
+    val dir: Int = 0x01
+    val deleted: Int = 0x02
+  }
+
+  def flagByte(fDir: Boolean, fDeleted: Boolean): Byte = {
+    import flags._
+    (
+      (if (fDir) dir else 0) |
+      (if (fDeleted) deleted else 0)
+    ).toByte
+  }
+
+  /** The block size in bytes. */
+  val blockSize: Int = 512
+
   /** Marker for FFS format. */
-  val format = 0x0FF5.toShort
+  val magic: Short = 0x0FF5.toShort
 
   /** Version of FFS format */
-  val version = 0.toByte
+  val version: Byte = 0.toByte
 
   /** An int as block address. */
   def address(a: Int) : Short = a.toShort
