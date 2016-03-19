9e69f65176d5727e1c243d0e7c23c7dbb90965fe
Justin Kaeser <github@accounts.justinkaeser.com>
2016-03-19 12:40:17 +0100

    better Path api

--- src/main/scala/ffs/impl/paths.scala
+++ src/main/scala/ffs/impl/paths.scala
@@ -3,7 +3,11 @@ package ffs.impl
 import ffs.common.constants
 
 
-case class Path(parts: Vector[String])
+case class Path(parts: Vector[String]) {
+  def isRoot = parts.isEmpty
+  def name = if (parts.isEmpty) "" else parts.last
+  def parent = if (parts.isEmpty) this else Path(parts.init)
+}
 
 object Path {
   // TODO this is still subject to all kinds of wonky user abuse. let's just assume user only uses mostly valid paths.
